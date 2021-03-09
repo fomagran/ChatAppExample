e243093f50a237ea4a522a78344610896742fabe
Fomagran <fomagran6@naver.com>
2021-03-09 12:56:29 +0900

    최근 메세지 만들기 수정

--- ChatAppExample/Model/Recent.swift
+++ ChatAppExample/Model/Recent.swift
@@ -32,9 +32,11 @@ func createRecent(members:[String],chatRoomId:String,withUserName:String,type:St
     var tempMembers = members
     
     reference(.Recent).whereField(kCHATROOMID, isEqualTo: chatRoomId).getDocuments { (snapshot, error) in
+        
         guard let snapshot = snapshot else {return}
         
         if !snapshot.isEmpty {
+            
             for recent in snapshot.documents {
                 let currentRecent = recent.data() as NSDictionary
                 if let currentUserId = currentRecent[kUSERID] {
@@ -43,18 +45,20 @@ func createRecent(members:[String],chatRoomId:String,withUserName:String,type:St
                         tempMembers.remove(at: tempMembers.firstIndex(of: currentUserId as! String)!)
                         
                     }
+                    
                 }
             }
             
-            for  userId in  tempMembers {
-                createRecentItems(userId: userId, chatRoomId: chatRoomId, members: members, withUserName: withUserName, type: type, users: users, avatarOfGroup: avatarOfGroup)
-            }
+        }
+        for  userId in  tempMembers {
+            createRecentItems(userId: userId, chatRoomId: chatRoomId, members: members, withUserName: withUserName, type: type, users: users, avatarOfGroup: avatarOfGroup)
         }
     }
     
 }
 
 func createRecentItems(userId:String,chatRoomId:String,members:[String],withUserName:String,type:String,users:[FUser]?,avatarOfGroup:String?) {
+    print("createRecentItems")
     let localReference = reference(.Recent).document()
     let recentId = localReference.documentID
     let date = dateFormatter().string(from: Date())
@@ -76,6 +80,7 @@ func createRecentItems(userId:String,chatRoomId:String,members:[String],withUser
             recent = [kRECENTID:recentId,kUSERID:userId,kCHATROOMID:chatRoomId,kMEMBERS:members,kMEMBERSTOPUSH:members,kWITHUSERFULLNAME:withUser!.fullname,kWITHUSERUSERID:withUser!.objectId,kLASTMESSAGE:"",kCOUNTER:0,kDATE:date,kTYPE:type,kAVATAR:withUser!.avatar] as [String:Any]
         }
     }
+    print(recent)
     
     localReference.setData(recent)
 }
